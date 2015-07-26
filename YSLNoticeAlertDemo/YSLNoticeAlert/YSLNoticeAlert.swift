2a5c5ea4e150891567650afbb58333b62327e595
y-hryk <dev.hy630823@gmail.com>
2015-07-27 00:28:09 +0900

    Initial commit

---
+++ YSLNoticeAlertDemo/YSLNoticeAlert/YSLNoticeAlert.swift
@@ -0,0 +1,21 @@
+//
+//  YSLNoticeAlert.swift
+//  YSLNoticeAlertDemo
+//
+//  Created by yamaguchi on 2015/07/24.
+//  Copyright (c) 2015年 h.yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class YSLNoticeAlert: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
